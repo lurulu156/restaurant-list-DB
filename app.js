112d2add072ad123df2d7a7872d330c1fef2c068
lurulu156 <kzzlin9@gmail.com>
2022-11-03 14:52:11 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,20 +1,40 @@
 const express = require('express')
 const app = express()
 const port = 3000
-
 const restaurantList = require('./restaurant.json')
-
 const exphbs = require('express-handlebars')
+const mongoose = require('mongoose')
+
+//set up handlebars
 app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
+//set mongoose and env
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
+//set DB connection status
+const db = mongoose.connection
+//DB connect fail
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+//DB connect success
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
 app.use(express.static('public'))
 
+//view all restaurants
 app.get('/', (req, res) => {
   const restaurants = restaurantList.results
   res.render('index', { restaurants: restaurants })
 })
 
+//view one restaurant
 app.get('/restaurants/:restaurant_id', (req, res) => {
   const restaurant = restaurantList.results.find((item) => {
     return item.id.toString() === req.params.restaurant_id
@@ -22,7 +42,7 @@ app.get('/restaurants/:restaurant_id', (req, res) => {
   res.render('show', { restaurant: restaurant })
 })
 
-
+//view search items
 app.get('/search', (req, res) => {
   const keyword = req.query.keyword
   const restaurants = restaurantList.results.filter((item) => {
@@ -32,6 +52,7 @@ app.get('/search', (req, res) => {
   res.render('index', { restaurants: restaurants, keyword: keyword })
 })
 
+//set listen port
 app.listen(port, () => {
   console.log(`Express is listening on localhost:${port}`)
 })
