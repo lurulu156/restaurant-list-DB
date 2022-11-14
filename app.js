27736483fa85cc41c8f9c17db19e1506ed45b07f
lurulu156 <kzzlin9@gmail.com>
2022-11-14 15:02:48 +0800

    feat: modify express routes

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const mongoose = require('mongoose')
 const Restaurant = require('./models/restaurant')
 const bodyParser = require('body-parser')
 
+// 引用路由器
+const routes = require('./routes')
+
+
 app.use(bodyParser.urlencoded({ extended: true }))
 
 //set up handlebars
@@ -28,112 +32,8 @@ db.once('open', () => {
 })
 
 app.use(express.static('public'))
-
-//view all restaurants with sorting function
-app.get('/', (req, res) => {
-  const sort = req.query.sort ? req.query.sort : { _id: 'asc' }
-  const sortMapping = {
-    'AtoZ': { name: 'asc' },
-    'ZtoA': { name: 'desc' },
-    'category': { category: 'asc' },
-    'location': { location: 'asc' }
-  }
-  Restaurant.find()
-    .lean()
-    .sort(sortMapping[sort])
-    .then(restaurants => res.render('index', { restaurants }))
-    .catch(err => console.log(err))
-})
-
-//create new item - GET
-app.get('/restaurants/new', (req, res) => {
-  return res.render('new')
-})
-app.post('/restaurants', (req, res) => {
-  const name = req.body.name
-  const name_en = req.body.name_en
-  const category = req.body.category
-  const image = req.body.image
-  const location = req.body.location
-  const phone = req.body.phone
-  const google_map = req.body.google_map
-  const rating = req.body.rating
-  const description = req.body.description
-  return Restaurant.create({ name, name_en, category, image, location, phone, google_map, rating, description })
-    .then(() => res.redirect('/'))
-    .catch(err => console.log(err))
-})
-
-//edit specific item
-app.get('/restaurants/:restaurant_id/edit', (req, res) => {
-  const id = req.params.restaurant_id
-  Restaurant.findById(id)
-    .lean()
-    .then(restaurant => res.render('edit', { restaurant }))
-    .catch(err => console.log(err))
-})
-app.post('/restaurants/:restaurant_id/edit', (req, res) => {
-  const id = req.params.restaurant_id
-  const name = req.body.name
-  const name_en = req.body.name_en
-  const category = req.body.category
-  const image = req.body.image
-  const location = req.body.location
-  const phone = req.body.phone
-  const google_map = req.body.google_map
-  const rating = req.body.rating
-  const description = req.body.description
-  return Restaurant.findById(id)
-    .then(restaurant => {
-      restaurant.name = name
-      restaurant.name_en = name_en
-      restaurant.category = category
-      restaurant.image = image
-      restaurant.location = location
-      restaurant.phone = phone
-      restaurant.google_map = google_map
-      restaurant.rating = rating
-      restaurant.description = description
-      return restaurant.save()
-    })
-    .then(() => res.redirect(`/restaurants/${id}`))
-    .catch(err => console.log(err))
-})
-
-//delete item
-app.post('/restaurants/:restaurant_id/delete', (req, res) => {
-    const id = req.params.restaurant_id
-    Restaurant.findById(id)
-      .then(restaurant => restaurant.remove())
-      .then(() => res.redirect('/'))
-      .catch(err => console.log(err))
-})
-
-//view one restaurant
-app.get('/restaurants/:restaurant_id', (req, res) => {
-  const id = req.params.restaurant_id
-  Restaurant.findById(id)
-    .lean()
-    .then(restaurant => res.render('show', { restaurant }))
-    .catch(err => console.log(err))
-})
-
-//view search items
-app.get('/search', (req, res) => {
-  const keyword = req.query.keyword
-  Restaurant.find()
-    .lean()
-    .then(data => data)
-    .then(data => {
-      const restaurants = data.filter((item) => {
-        return item.name.toLowerCase().includes(req.query.keyword.toLowerCase()) ||
-          item.category.toLowerCase().includes(req.query.keyword.toLowerCase())
-      })
-      res.render('index', { restaurants: restaurants, keyword: keyword })
-    }
-    )
-    .catch(err => console.log(err))
-})
+// 將 request 導入路由器
+app.use(routes)
 
 //set listen port
 app.listen(port, () => {
---
+++ routes/modules/home.js
@@ -0,0 +1,39 @@
+// 引用 Express 與 Express 路由器
+const express = require('express')
+const router = express.Router()
+// 引用 Todo model
+const Restaurant = require('../../models/restaurant')
+// 定義首頁路由
+router.get('/', (req, res) => {
+  const sort = req.query.sort ? req.query.sort : { _id: 'asc' }
+  const sortMapping = {
+    'AtoZ': { name: 'asc' },
+    'ZtoA': { name: 'desc' },
+    'category': { category: 'asc' },
+    'location': { location: 'asc' }
+  }
+  Restaurant.find()
+    .lean()
+    .sort(sortMapping[sort])
+    .then(restaurants => res.render('index', { restaurants }))
+    .catch(err => console.log(err))
+})
+
+//view search items
+router.get('/search', (req, res) => {
+  const keyword = req.query.keyword
+  Restaurant.find()
+    .lean()
+    .then(data => data)
+    .then(data => {
+      const restaurants = data.filter((item) => {
+        return item.name.toLowerCase().includes(req.query.keyword.toLowerCase()) ||
+          item.category.toLowerCase().includes(req.query.keyword.toLowerCase())
+      })
+      res.render('index', { restaurants: restaurants, keyword: keyword })
+    }
+    )
+    .catch(err => console.log(err))
+})
+// 匯出路由模組
+module.exports = router
